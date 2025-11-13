ed678bdb7f3b055f4d08f7d06d7578b9f6d2887d
Shayan Haghighi <shayan.haghighi04@gmail.com>
2025-11-13 14:39:18 +0000

    inital commit

---
+++ nvim/lua/plugins/lsp.lua
@@ -0,0 +1,129 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = { "lua", "python", "javascript", "typescript", "html", "css" },
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end,
+  },
+  { "neovim/nvim-lspconfig" },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "L3MON4D3/LuaSnip",
+    },
+    config = function()
+      -- ===== Autocompletion (cmp) =====
+      local cmp = require("cmp")
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        mapping = {
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.confirm({ select = true })
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<CR>"] = cmp.mapping.confirm({ select = false }),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-n>"] = cmp.mapping.select_next_item(),
+          ["<C-p>"] = cmp.mapping.select_prev_item(),
+        },
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "buffer" },
+          { name = "path" },
+        }),
+      })
+
+      -- ===== LSP Setup (new API) =====
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      vim.lsp.config.lua_ls = {
+        cmd = { "lua-language-server" },
+        capabilities = capabilities,
+        settings = {
+          Lua = { diagnostics = { globals = { "vim" } } },
+        },
+      }
+
+      vim.lsp.config.ts_ls = {
+        cmd = { "typescript-language-server", "--stdio" },
+        capabilities = capabilities,
+      }
+
+      vim.lsp.config.pyright = {
+        cmd = { "pyright-langserver", "--stdio" },
+        capabilities = capabilities,
+      }
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "lua", "javascript", "typescript", "python" },
+        callback = function()
+          local cfg = vim.lsp.config[vim.bo.filetype .. "_ls"]
+          if cfg then
+            vim.lsp.start(cfg)
+          end
+        end,
+      })
+    end,
+  },
+  { "williamboman/mason.nvim", config = true },
+
+  {
+    "HiPhish/nvim-ts-rainbow2",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        highlight = { enable = true },
+        rainbow = {
+          enable = true,
+          strategy = require("ts-rainbow.strategy.global"),
+          query = { "rainbow-parens", html = "rainbow-tags" },
+        },
+      })
+    end,
+  },
+
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    opts = {
+      indent = { char = "│" },
+      scope = { enabled = true },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters = {
+        stylua = { prepend_args = { "--indent-type", "Spaces", "--indent-width", "2" } },
+      },
+      formatters_by_ft = {
+        lua = { "stylua" },
+        -- Conform will run multiple formatters sequentially
+        --python = { "isort", "black" },
+        -- You can customize some of the format options for the filetype (:help conform.format)
+        --rust = { "rustfmt", lsp_format = "fallback" },
+        -- Conform will run the first available formatter
+        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+      },
+      format_on_save = {
+        timeout_ms = 1000,
+        lsp_fallback = true,
+      },
+    },
+  },
+}
---
+++ nvim/lua/plugins/ui.lua
@@ -0,0 +1,152 @@
+return {
+  {
+    "nvim-tree/nvim-tree.lua",
+    config = function()
+      local nvim_tree = require("nvim-tree")
+
+      local function on_attach(bufnr)
+        local api = require("nvim-tree.api")
+
+        -- default mappings
+        api.config.mappings.default_on_attach(bufnr)
+
+        -- TODO: fix
+        vim.keymap.set("n", "<leader>t", api.tree.focus, { buffer = bufnr, desc = "Focus NvimTree" })
+      end
+
+      nvim_tree.setup({
+        on_attach = on_attach,
+        update_cwd = true,
+        update_focused_file = {
+          enable = true,
+          update_cwd = true,
+        },
+        hijack_cursor = true,
+        view = {
+          side = "left",
+          width = 35,
+        },
+        renderer = {
+          highlight_git = true, -- color git status
+          highlight_opened_files = "all",
+          highlight_modified = "icon",
+          root_folder_label = ":t", -- shorten root folder name
+          full_name = false,
+          indent_markers = { enable = true },
+          add_trailing = false,
+          icons = {
+            webdev_colors = true,
+            git_placement = "after",
+            show = {
+              file = true,
+              folder = true,
+              folder_arrow = true,
+              git = true,
+            },
+            glyphs = {
+              default = "󰈙",
+              symlink = "",
+              folder = {
+                arrow_closed = "",
+                arrow_open = "",
+                default = "",
+                open = "",
+                empty = "",
+                empty_open = "",
+                symlink = "",
+              },
+              git = {
+                unstaged = "M",
+                staged = "S",
+                unmerged = "",
+                renamed = "➜",
+                untracked = "★",
+                deleted = "",
+                ignored = "◌",
+              },
+            },
+          },
+        },
+      })
+
+      vim.cmd([[
+  hi NvimTreeGitNewIcon guifg=#ff4444 gui=bold
+
+  hi NvimTreeGitStagedIcon guifg=#00ff7f gui=bold
+
+  hi NvimTreeGitDirtyIcon guifg=#ffb347 gui=bold
+    ]])
+    end,
+  },
+  {
+    "nvim-tree/nvim-web-devicons",
+    lazy = false, -- load immediately so icons are available everywhere
+    priority = 1000,
+    config = function()
+      require("nvim-web-devicons").setup({
+        override = {},
+        color_icons = true,
+        default = true, -- show default icons for files with no specific icon
+        strict = true, -- match exact file extensions
+      })
+    end,
+  },
+
+  {
+    "nvim-lualine/lualine.nvim",
+    config = function()
+      require("lualine").setup({ options = { theme = "auto" } })
+    end,
+  }, -- nvim status line
+  { -- theme
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd([[colorscheme tokyonight]])
+    end,
+  },
+  { -- bufferlines (tabs)
+    "akinsho/bufferline.nvim",
+    version = "*",
+    dependencies = "nvim-tree/nvim-web-devicons",
+    config = function()
+      require("bufferline").setup({
+        options = {
+          diagnostics = "nvim_lsp",
+          separator_style = "slant", -- "thin", "slant", or "padded_slant"
+          show_buffer_close_icons = false,
+          show_close_icon = false,
+          always_show_bufferline = true,
+        },
+      })
+
+      -- Optional keymaps for tab switching
+      vim.keymap.set("n", "<Tab>", "<Cmd>BufferLineCycleNext<CR>", { desc = "Next tab" })
+      vim.keymap.set("n", "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", { desc = "Prev tab" })
+    end,
+  },
+  { -- smart window tiling
+    "mrjones2014/smart-splits.nvim",
+    config = function()
+      local splits = require("smart-splits")
+
+      -- Movement between splits
+      vim.keymap.set("n", "<C-h>", splits.move_cursor_left)
+      vim.keymap.set("n", "<C-j>", splits.move_cursor_down)
+      vim.keymap.set("n", "<C-k>", splits.move_cursor_up)
+      vim.keymap.set("n", "<C-l>", splits.move_cursor_right)
+
+      -- Resizing splits
+      vim.keymap.set("n", "<A-h>", splits.resize_left)
+      vim.keymap.set("n", "<A-j>", splits.resize_down)
+      vim.keymap.set("n", "<A-k>", splits.resize_up)
+      vim.keymap.set("n", "<A-l>", splits.resize_right)
+
+      vim.keymap.set("n", "<leader><leader>h", require("smart-splits").swap_buf_left)
+      vim.keymap.set("n", "<leader><leader>j", require("smart-splits").swap_buf_down)
+      vim.keymap.set("n", "<leader><leader>k", require("smart-splits").swap_buf_up)
+      vim.keymap.set("n", "<leader><leader>l", require("smart-splits").swap_buf_right)
+    end,
+  },
+}
