49ff92dc9ecd8bd92fb2389d72952c289119becf
Shayan Haghighi <shayan.haghighi04@gmail.com>
2025-11-14 12:30:39 +0000

    modularise bashrc

---
+++ bash/aliases.bash
@@ -0,0 +1,40 @@
+
+# enable color support of ls and also add handy aliases
+if [ -x /usr/bin/dircolors ]; then
+    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    alias ls='ls --color=auto'
+    #alias dir='dir --color=auto'
+    #alias vdir='vdir --color=auto'
+
+    alias grep='grep --color=auto'
+    alias fgrep='fgrep --color=auto'
+    alias egrep='egrep --color=auto'
+fi
+
+# colored GCC warnings and errors
+#export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
+# some more ls aliases
+alias la='ls -a'
+alias l='ls'
+alias ls='eza --group-directories-last --sort=Extension -1'
+alias lt='eza -T -L 5 --git-ignore'
+alias ll='eza --group-directories-last -l --git --sort=modified'
+
+alias cat='batcat'
+alias catp='batcat --paging=always'
+
+alias fkill='kill -9 $(ps aux | fzf | awk "{print $2}")'
+
+alias fcdr='fcd -r'
+alias fcdra='fcd -r -a'
+alias fcda='fcd -a'
+
+alias python=python3
+alias ...='cd ../..'
+alias webstorm='gtk-launch webstorm.desktop'
+alias idea='/home/shayan/Apps/idea-IU-252.27397.103/bin/idea'
+alias copy='xclip -selection clipboard'
+alias fd="fdfind"
+# Add an "alert" alias for long running commands.  Use like so:
+#   sleep 10; alert
+alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
---
+++ bash/ui.bash
@@ -0,0 +1,17 @@
+
+CFG_DIR=/home/shayan/.config/
+
+# setup eza themes
+export EZA_ICONS_AUTO=always
+export EZA_CONFIG_DIR="$CFG_DIR/eza"
+
+# starship initialization
+eval "$(starship init bash)"
+STARSHIP_SRC="$CFG_DIR/starship"
+eval "$(mustache $STARSHIP_SRC/starship_vars.json $STARSHIP_SRC/starship.mustache > $CFG_DIR/starship.toml)"
+export STARSHIP_CONFIG="$CFG_DIR/starship.toml"
+
+# LS_COLORS interferes with eza themes, so unset
+unset LS_COLORS
+
+export VISUAL=nvim
